10716446cfc2d8a57c4fa895705fe1e905813677
LAPTOP-C6IOEE1R <jhennebo@gmail.com>
2022-12-09 00:28:22 +0100

    panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Seance;
+use App\Repository\SeanceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/cart', name: 'cart_')]
+class CartController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, SeanceRepository $seanceRepository): Response
+    {
+        $cart = $session->get("cart", []);
+        $total = 0;
+        // on fabrique les données
+        $dataCart = [] ;
+
+       foreach($cart as $id => $quantite){
+           $seance = $seanceRepository->find($id);
+        //   dd($seance);
+         $dataCart[] = [
+              "activites" => $seance,
+               "quantite" => $quantite
+          ];
+           $total += $seance->getPrice() * $quantite;
+      }
+
+        return $this->render('cart/index.html.twig', compact("dataCart", "total"));
+    }
+
+ // #[Route('/add/{id<[0-9]+>}', name: 'add')]
+ //  public function add(Seance $seance, SessionInterface $session)
+//   {
+      // on récupère le panier actuel
+  //    $cart = $session->get("cart", []);
+ //     $id = $seance->getId();
+
+  //   if(!empty($cart[$id])){
+   //    $cart[$id]++;
+   //    }else{
+  //    $cart[$id] = 1;
+   //    }
+      //  on sauvegarde dans la session
+   //   $session->set("cart", $cart);
+   //     dd($session);
+   // }
+
+  #[Route('/add/{id<[0-9]+>}', name: 'add')]
+    public function add($id, SessionInterface $session)
+    {
+       // on récupère le panier actuel
+      $cart = $session->get("cart", []);
+    
+     if(!empty($cart[$id])){
+       $cart[$id]++;
+      }else{
+       $cart[$id] = 1;
+      }
+       //on sauvegarde dans la session
+      $session->set("cart", $cart);
+        
+       return $this->redirectToRoute('cart_index');
+    }
+
+
+}
---
+++ src/Entity/Seance.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeanceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeanceRepository::class)]
+class Seance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $datedelaseance = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getDatedelaseance(): ?\DateTimeInterface
+    {
+        return $this->datedelaseance;
+    }
+
+    public function setDatedelaseance(\DateTimeInterface $datedelaseance): self
+    {
+        $this->datedelaseance = $datedelaseance;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
